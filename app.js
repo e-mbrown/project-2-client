2af7f7c698ca0332b627b4384f19caac3cd9fb99
e-mbrown <ebsonari@gmail.com>
2020-08-04 13:19:27 -0400

    Add button is nearly ready

--- app.js
+++ app.js
@@ -4,18 +4,19 @@ const URL = deployedURL ? deployedURL : "http://localhost:3000";
 
 //GLOBAL VARIABLES
 const $container = $('.container');
-const $addinput = $('#fetch');
+const $addinput = $('#submit')
+const $openForm= $('.fetch');
 const $editButton = $('#edit');
 const $del = $('#del');
 
 ////FUNCTIONS
 const getData = async () =>{
-    //API CALL
+    //  API CALL
     ///////////////
     const response = await fetch(`${URL}/art`);
     const data = await response.json();
     //Populate Container with images
-    ///////////////////////////////
+    
     data.forEach((piece) => {
     const $link = $('<a>').attr('href', '#exampleModal').attr('data-toggle', 'modal')
     const $image = $('<img>').attr('src', piece.url);
@@ -24,7 +25,7 @@ const getData = async () =>{
     $container.append($link);
     });
 };
-
+//  DISPLAY IMAGES WITH NO FILTER FOR HOMEPAGE
 const fillModal = (imageData) => {
     $('#info').empty();
     $('#img').empty();
@@ -49,4 +50,92 @@ const fillModal = (imageData) => {
 
 };
 
+////  ADD ART
+
+$openForm.on('click', () => openForm())
+const openForm = () =>{
+    $('#choice').toggle(700);
+    
+};
+//Make fields appear
+
+const getOptions = (choice) =>{
+    $('.input-group').css('display', 'none')
+
+    if(choice == 'Comic'){
+        $('#dDiv').toggle(700);
+        $('#nDiv').toggle(700);
+        $('#mDiv').toggle(700);
+        $('#uDiv').toggle(700);
+        $('#pDiv').toggle(700);
+        $('#submit').toggle(700);
+    }
+    else if(choice == 'Illustration'){
+        $('#dDiv').toggle(700);
+        $('#nDiv').toggle(700);
+        $('#mDiv').toggle(700);
+        $('#uDiv').toggle(700);
+        $('#cDiv').toggle(700);
+        $('#submit').toggle(700);
+    }
+    else{
+        $('#dDiv').toggle(700);
+        $('#nDiv').toggle(700);
+        $('#mDiv').toggle(700);
+        $('#uDiv').toggle(700);
+        $('#submit').toggle(700);
+    };
+   
+}
+
+$('select#choice').on('change', () => {
+    x = $('#choice').val();
+    getOptions(x);
+});
+
+//COLLECT DATA FROM CREATE FORM
+
+$addinput.on('click', () => createArt());
+
+const createArt = async () => {
+    x = $('#choice').val();
+    let newArt =''
+    if(x == 'Illustration'){
+        newArt ={
+            name: $('#name').val(),
+            medium: $('#medium').val(),
+            artType: x,
+            description: $('#description').val(),
+            url: $('#url').val(),
+            context: $('#context').val()
+
+        }
+    }
+    else if (x == 'Comic'){
+        newArt ={
+            name: $('#name').val(),
+            medium: $('#medium').val(),
+            description: $('#description').val(),
+            url: $('#url').val(),
+            page: $('#page').val(),
+            genre: $('#genre').val()
+        }
+    }
+    else {
+        newArt ={
+            name: $('#name').val(),
+            medium: $('#medium').val(),
+            description: $('#description').val(),
+            url: $('#url').val()
+        }
+    }
+//SEND REQUEST TO API  
+    console.log(newArt)
+}
+
+
+
+
+
+/////////////////////////////////
 getData();
